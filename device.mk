43b7619d32386d5c4be72cbe9082256ed97f16c9
xilyray <xilyray@gmail.com>
2021-12-07 07:14:21 +0000

    X01BD: Enable QTI THERMAL flags

--- device.mk
+++ device.mk
@@ -25,6 +25,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.device.rc
 
+# Thermal
+TARGET_USE_QTI_THERMAL_SERVICE := true
+
 # Thermal configs
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
